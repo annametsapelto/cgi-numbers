5d665033c890facac61c9ebc9b5e8a26f3e805ae
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-06-20 21:22:21 +0300

    Create methods

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        List<int> numbers = getRandomNumbers();
+        int firstNumber = compareNumbers(numbers[0], numbers[1]);
+        int secondNumber = compareNumbers(numbers[1], numbers[2]);
+        system.out.println(getBiggest(first, second));
+    }
+
+    public ArrayList<int> getRandomNumbers() {
+        List<int> numbers = new ArrayList<int>();
+        //randomize
+        return numbers;
+    }
+
+    public int compareNumbers(int first, int second) {
+
+    }
+
+    public int getBiggest(int first, int second) {
+        
+    }
+}
